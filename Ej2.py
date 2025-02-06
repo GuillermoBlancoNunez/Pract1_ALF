159d9c310789579d54c24fb051c097ff1d692de6
GuillermoBlancoNunez <guillermo.blanco.nunez@udc.es>
2025-02-06 23:15:46 +0100

    Finished first draft for Ej2.

--- Ej2.py
+++ Ej2.py
@@ -8,13 +8,43 @@ def axioma(i: int)->str:
     return (i * "~") + "m~g" + (i * "~") + "~"
 
 
-def mg(n: int):
+def mg(n: int)->list:
     coleccion = []
-    
-    return 
+    orden = []
+    i = 0
+    while len(coleccion) < n:
+        s = axioma(i)
+        coleccion.append(s)
+        orden.append(s)
+        c = producir(orden.pop(0))
+        coleccion.append(c)
+        orden.append(c)
+        i += 1
+
+
+    return coleccion
+
+def pedirNum()->int:
+    while True:
+        try:
+            n = int(input("\nIngrese la cantidad de teoremas a generar: "))
+
+                        
+        except ValueError:
+            print("\nError: Debe ingresar un número válido.")
+        else:
+            return n
+
+        
+
 def main()->None:
-    coleccion = []
+    n = pedirNum()
+    teoremas = mg(n)
+    for i in range(n):
+        print(teoremas[i])
+    correct = ["m∼g∼", "m∼∼g∼∼", "∼m∼g∼∼", "m∼∼∼g∼∼∼", "∼m∼∼g∼∼∼", "∼∼m∼g∼∼∼", "m∼∼∼∼g∼∼∼∼", "∼m∼∼∼g∼∼∼∼", "∼∼m∼∼g∼∼∼∼", "∼∼∼m∼g∼∼∼∼", "m∼∼∼∼∼g∼∼∼∼∼", "∼m∼∼∼∼g∼∼∼∼∼", "∼∼m∼∼∼g∼∼∼∼∼", "∼∼∼m∼∼g∼∼∼∼∼", "∼∼∼∼m∼g∼∼∼∼∼"]
 
 
 if __name__ == "__main__":
     main()
+    ["m∼g∼", "m∼∼g∼∼", "∼m∼g∼∼", "m∼∼∼g∼∼∼", "∼m∼∼g∼∼∼", "∼∼m∼g∼∼∼", "m∼∼∼∼g∼∼∼∼", "∼m∼∼∼g∼∼∼∼", "∼∼m∼∼g∼∼∼∼", "∼∼∼m∼g∼∼∼∼", "m∼∼∼∼∼g∼∼∼∼∼", "∼m∼∼∼∼g∼∼∼∼∼", "∼∼m∼∼∼g∼∼∼∼∼", "∼∼∼m∼∼g∼∼∼∼∼", "∼∼∼∼m∼g∼∼∼∼∼"]
