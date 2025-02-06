e7cb321628277cc6ef479ec47f038d948f2e8679
GuillermoBlancoNunez <guillermo.blanco.nunez@udc.es>
2025-02-06 20:09:25 +0100

    Created producing and axiom functions for Ej2.

--- Ej2.py
+++ Ej2.py
@@ -1,2 +1,20 @@
+def producir(cad:str)->str:
+    for i in cad:
+        if i == "g":
+            cad = cad[:cad.index(i)] + "~" + cad[cad.index(i):] + "~"
+    return cad
+
+def axioma(i: int)->str:
+    return (i * "~") + "m~g" + (i * "~") + "~"
+
+
+def mg(n: int):
+    coleccion = []
+    
+    return 
 def main()->None:
-    return
+    coleccion = []
+
+
+if __name__ == "__main__":
+    main()
