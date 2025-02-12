a7cc606ef8b17d4a33da1f7b6e7386585ecbf40e
GuillermoBlancoNunez <guillermo.blanco.nunez@udc.es>
2025-02-12 17:22:44 +0100

    Minor chnge on Ej3 and finished all functionalities on Ej2.

--- Ej2.py
+++ Ej2.py
@@ -1,8 +1,6 @@
 def producir(cad:str)->str:
-    for i in cad:
-        if i == "g":
-            cad = cad[:cad.index(i)] + "~" + cad[cad.index(i):] + "~"
-    return cad
+    return cad[:cad.index("g")] + "~" + cad[cad.index("g"):] +"~"
+    
 
 def axioma(i: int)->str:
     return (i * "~") + "m~g" + (i * "~") + "~"
@@ -21,28 +19,56 @@ def mg(n: int)->list:
         coleccion.append(c)
         orden.append(c)
 
-
-
     return coleccion
 
-def pedirNum()->int:
+def pedirNumCantidad()->int:
     while True:
         try:
             n = int(input("\nIngrese la cantidad de teoremas a generar: "))
 
-                        
         except ValueError:
             print("\nError: Debe ingresar un número válido.")
         else:
             return n
 
+def mgCheck(cad:str):
+    
+    i = cad.index("m")
+    if cad == axioma(len(cad[:i])):
+        return "yes"
+    elif len(cad) > 4 and len(cad)% 2 == 0:
+        newCad = cad[:cad.index("g")-1] + cad[cad.index("g"):len(cad)-1]
+        return mgCheck(newCad)
         
+    return "no"
+
+
+def pedirNumFuncion():
+    while True:
+        try:
+            n = int(input("\nIngrese la funcionalidad del ejercicio que quiere utilizar (1-2):\n1. Generar n número de modelos.\n2. Comprobrar si un posible teorema forma parte del sistema formal mg~\n "))
+            if n in [1, 2]:
+                break
+            else:
+                print("\nError: Debe introducir un número entre 1 y 2.")
+        except ValueError:
+            print("\nError: Debe ingresar un número válido.")
+
+    return n
+
 
 def main()->None:
-    n = pedirNum()
-    teoremas = mg(n)
-    for i in range(n):
-        print(teoremas[i])
+    f = pedirNumFuncion()
+    if f == 1:
+        n = pedirNumCantidad()
+        teoremas = mg(n)
+        for i in range(n):
+            print(teoremas[i])
+    elif f ==2:
+        s = str(input("Introduzca la cadena que quiera comprobar si está en el sistema formal mg~;\n"))
+        print(mgCheck(s))
+            
+
 
 if __name__ == "__main__":
     main()
